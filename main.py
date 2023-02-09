20e562173352b9683e35ae03e25c8c1b20b7ad34
Mateus Luis <mateusoliveirap@hotmail.com>
2023-02-09 15:47:21 -0300

    Co-authored-by: Júlia Belo <Menzilith@users.noreply.github.com> Co-authored-by: XXNumbedXX <XXNumbedXX@users.noreply.github.com> Co-authored-by: Menzilith <Menzilith@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-class Jogador():
-    
-    mo
-    def __init__(jogador,):
-        self.joogador=null
-        moedas
+class Jogador:
+
+    def __init__(self, jogador):
+        string self.jogador = jogador
+        self.moedas = 2
+    def bloqueio():
+        print("bloqueio")
+        
+    def contesta():
+        print("contesta")
+    def renda():
+        pass
+    def ajuda_externa():
+        pass
+    def perder_moeda():
+        pass
+    def golpe_de_estado():
+        
+
+class Juiz:
+    pass
+
     
 
         
